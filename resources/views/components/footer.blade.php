91debdccb6ba83824c9be777cd63052b7e881d41
Szabo Patrik <patrik.szabo@gpstuner.com>
2025-03-16 09:12:15 +0100

    Fix footer mobile view

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,12 +1,12 @@
 <footer class="bg-red-700 text-white py-8">
-    <div class="container mx-auto flex flex-col md:flex-row justify-between">
+    <div class="container mx-auto px-4 md:px-0 flex flex-col md:flex-row justify-between">
         <div class="mb-4 md:mb-0">
             <h3 class="font-bold">LP Solutions</h3>
-            <p>Creatio © {{now()->format('Y')}}</p>
+            <p>Creatio © {{ now()->format('Y') }}</p>
         </div>
         <nav class="flex space-x-4">
-            <a href="{{route('home')}}" class="hover:text-red-300 text-white">Főoldal</a>
-            <a href="{{route('creatio-products')}}" class="hover:text-red-300 text-white">Termékeink</a>
+            <a href="{{ route('home') }}" class="hover:text-red-300 text-white">Főoldal</a>
+            <a href="{{ route('creatio-products') }}" class="hover:text-red-300 text-white">Termékeink</a>
         </nav>
     </div>
 </footer>
