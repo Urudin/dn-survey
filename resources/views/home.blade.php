b2dd0d8105f2ceaf3c3c19fe2113727b0f0fe749
Szabo Patrik <patrik.szabo@gpstuner.com>
2025-05-15 16:19:10 +0200

    og tags

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,8 +10,7 @@
 
     {{--CONTACT FORM--}}
 
-    <section class="py-12 relative overflow-hidden"
-             style="background-image: url('{{ asset('/images/contahct-bg.jpg') }}'); background-size: cover; background-position: center;">
+    <section class="py-12 relative overflow-hidden">
         <div class="container md:mt-32 mx-auto w-full md:w-[80%] lg:w-[70%] xl:w-[60%]">
             <div class="w-full">
                 <x-contact-form class="w-full"></x-contact-form>
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -3,7 +3,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dragon-Network</title>
+    <title>The Dragon-Network Survey</title>
+    <meta property="og:title"
+          content="The Dragon-Network Survey">
+    <meta name="description" content="Make DN great again!">
+    <meta property="og:description" content="Make DN great again!">
+
+    <meta name="keywords" content="Lineage L2 Substack Dragon Network">
+
+    <!-- Open Graph Meta Tags -->
+    <meta property="og:type" content="website">
+    <meta property="og:image" content="{{ asset('images/header.jpg') }}">
+    <meta property="og:url" content="{{ url()->current() }}">
+
     <script src="https://www.google.com/recaptcha/api.js" async defer></script>
     <script src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js" defer></script>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
